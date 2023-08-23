0067814e9f0ae8c2114ec9b2ea84a9c202c6a559
Jonathan <many.d.r.o.2001@hotmail.com>
2023-08-23 15:22:47 -0500

    An early version of the game has been completed

--- script.js
+++ script.js
@@ -6,9 +6,78 @@
 //4. If it is the CPU randomize the result
 //5. Validating the result 
 //6. Starting over
+//7. Give the option of the best of 3
 
+
+// Game Configuration
 const options = {
     'rock': 1,
-    'scissors': 2,
-    'paper': 3
-}; 
+    'paper': 2,
+    'scissors': 3
+  };
+
+// DOM Elements
+const playerStatus = document.querySelector('.player__status-txt');
+const buttons = document.querySelectorAll('.player__choice-btn');
+const roundTxt = document.querySelector('.round');
+const scoreTxt = document.querySelectorAll('.player__score');
+const resultTxt = document.querySelector('.result');
+const player2ChoiceTxt = document.querySelector('.player__choice--2');
+
+// Game State
+let player1Choice, player2Choice, scores, result, round;
+
+// Event Listener for Button Clicks
+buttons.forEach(button => button.addEventListener("click", makeChoice));
+
+// Function to Handle Player Choice and Evaluate Round
+function makeChoice(e) {
+    roundTxt.innerText = `Round ${round}`;
+    player1Choice = options[e.target.id];
+    playerStatus.innerText = `You chose ${e.target.id}`;
+    player2Play();
+    evaluate();
+}
+
+// Evaluate the Round Result
+function evaluate() {
+    if (
+        (player1Choice === 1 && player2Choice === 3) ||
+        (player1Choice === 2 && player2Choice === 1) ||
+        (player1Choice === 3 && player2Choice === 2)
+    ) {
+        scores[0]++;
+        result = 'win';
+    } else if (player1Choice === player2Choice) {
+        result = 'tie';
+    } else {
+        scores[1]++;
+        result = 'lose';
+    }
+    round++;
+    resultTxt.innerText = `You ${result}`;
+    scoreTxt[0].innerText = scores[0];
+    scoreTxt[1].innerText = scores[1];
+}
+
+// Initialize the Game
+function initializeGame() {
+    scores = [0, 0];
+    result = '';
+    round = 1;
+    roundTxt.innerText = `Round ${round}`;
+}
+
+// Get a Random Choice for Player 2
+function player2Play() {
+    player2Choice = getRandomInt(1, 3);
+    player2ChoiceTxt.innerText = Object.keys(options)[player2Choice - 1];
+}
+
+// Function for getting a random number with a range
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Initialize the game when the script loads
+initializeGame();
