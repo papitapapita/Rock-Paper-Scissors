f61fc46a8aac09882ca617b271a982ce017d2e30
Jonathan <many.d.r.o.2001@hotmail.com>
2023-08-23 03:46:07 -0500

    Basic structure of the HTMLcreated

---
+++ script.js
@@ -0,0 +1,14 @@
+//1. Having two players
+//1.1 Playing with CPU
+//1.2 Playing online with a friend
+//2. Creating the paper, rock and scissors object
+//3. Getting the input of the player
+//4. If it is the CPU randomize the result
+//5. Validating the result 
+//6. Starting over
+
+const options = {
+    'rock': 1,
+    'scissors': 2,
+    'paper': 3
+}; 
