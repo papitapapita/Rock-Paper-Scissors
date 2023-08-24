38b89806d54d070757cf35e30789991678758081
Jonathan <many.d.r.o.2001@hotmail.com>
2023-08-24 03:11:10 -0500

    Implemented the best of 3 and improvement in the code readability

--- script.js
+++ script.js
@@ -10,11 +10,12 @@
 
 
 // Game Configuration
-const options = {
-    'rock': 1,
-    'paper': 2,
-    'scissors': 3
-  };
+const options = ['rock', 'paper', 'scissors'];
+const winRules = {
+  'rock': 'scissors',
+  'paper': 'rock',
+  'scissors': 'paper'
+};
 
 // DOM Elements
 const playerStatus = document.querySelector('.player__status-txt');
@@ -23,45 +24,55 @@ const roundTxt = document.querySelector('.round');
 const scoreTxt = document.querySelectorAll('.player__score');
 const resultTxt = document.querySelector('.result');
 const player2ChoiceTxt = document.querySelector('.player__choice--2');
+const restartBtn = document.querySelector('.restart-btn');
 
 // Game State
-let player1Choice, player2Choice, scores, result, round;
+let player1Choice, player2Choice, scores, result, round, gameOver;
 
 // Event Listener for Button Clicks
-document.querySelector('.player__choice').addEventListener("click", (e) => {
-    if (e.target && e.target.matches(".player__choice-btn")) {
-        makeChoice(e.target.id);
-    }
-});
+document.querySelector('.player__choice').addEventListener("click", handleButtonClick);
+restartBtn.addEventListener("click", initializeGame);
+
+// Function to Handle Button Click
+function handleButtonClick(e) {
+  if (e.target && e.target.matches(".player__choice-btn")) {
+    makeChoice(e.target.id);
+  }
+}
 
 // Function to Handle Player Choice and Evaluate Round
-function makeChoice(e) {
+function makeChoice(choice) {
+    if(gameOver){
+        return;
+    }
     roundTxt.innerText = `Round ${round}`;
-    player1Choice = options[e];
-    playerStatus.innerText = `You chose ${e}`;
+    player1Choice = options.indexOf(choice) + 1;
+    playerStatus.innerText = `You chose ${choice}`;
     player2Play();
     evaluate();
 }
 
 // Evaluate the Round Result
 function evaluate() {
-    if (
-        (player1Choice === 1 && player2Choice === 3) ||
-        (player1Choice === 2 && player2Choice === 1) ||
-        (player1Choice === 3 && player2Choice === 2)
-    ) {
+    if (scores[0] >= 3 || scores[1] >= 3) {
+        result = scores[0] >= 3 ? 'won the match' : 'lost the match';
+        gameOver = true;
+        restartBtn.style.display = 'block';
+    }else{
+        const player1ChoiceName = options[player1Choice - 1];
+        if (player1ChoiceName === player2ChoiceTxt.innerText) {
+        result = 'tie';
+        } else if (winRules[player1ChoiceName] === player2ChoiceTxt.innerText) {
         scores[0]++;
         result = 'win';
-    } else if (player1Choice === player2Choice) {
-        result = 'tie';
-    } else {
+        } else {
         scores[1]++;
         result = 'lose';
+        }
+        round++;
+        updateScoreDisplay();
     }
-    round++;
     resultTxt.innerText = `You ${result}`;
-    scoreTxt[0].innerText = scores[0];
-    scoreTxt[1].innerText = scores[1];
 }
 
 // Initialize the Game
@@ -69,13 +80,25 @@ function initializeGame() {
     scores = [0, 0];
     result = '';
     round = 1;
+    gameOver = false;
     roundTxt.innerText = `Round ${round}`;
+    restartBtn.style.display = 'none';
+    updateScoreDisplay();
+    resultTxt.innerText = '';
+    player2ChoiceTxt.innerText = '';
+    playerStatus.innerText = '';
+}
+
+// Update the Score Display
+function updateScoreDisplay() {
+    scoreTxt[0].innerText = scores[0];
+    scoreTxt[1].innerText = scores[1];
 }
 
 // Get a Random Choice for Player 2
 function player2Play() {
     player2Choice = getRandomInt(1, 3);
-    player2ChoiceTxt.innerText = Object.keys(options)[player2Choice - 1];
+    player2ChoiceTxt.innerText = options[player2Choice - 1];
 }
 
 // Function for getting a random number with a range
